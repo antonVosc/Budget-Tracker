36b9d115628d9973d6577a0e9109b4899c6d39cd
antonVosc <anton.vosc@gmail.com>
2025-04-11 23:43:21 +0100

    Added income, expense, and balance info on graph and started to work on Manage page (allow to select currency and started to implement categories overview)

--- app/(dashboard)/_components/History.tsx
+++ app/(dashboard)/_components/History.tsx
@@ -6,9 +6,11 @@ import { Badge } from "@/components/ui/badge";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { GetFormatterForCurrency } from "@/lib/helpers";
 import { Period, Timeframe } from "@/lib/types";
+import { cn } from "@/lib/utils";
 import { UserSettings } from "@prisma/client";
 import { useQuery } from "@tanstack/react-query";
-import React, { useMemo, useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
+import CountUp from "react-countup";
 import {
   Bar,
   BarChart,
@@ -187,7 +189,12 @@ function History({ userSettings }: { userSettings: UserSettings }) {
                     className="cursor-pointer"
                   />
 
-                  <ToolTip cursor={{ opacity: 0.1 }} />
+                  <ToolTip
+                    cursor={{ opacity: 0.1 }}
+                    content={(props) => (
+                      <CustomTooltip formatter={formatter} {...props} />
+                    )}
+                  />
                 </BarChart>
               </ResponsiveContainer>
             )}
@@ -208,3 +215,82 @@ function History({ userSettings }: { userSettings: UserSettings }) {
 }
 
 export default History;
+
+function CustomTooltip({ active, payload, formatter }: any) {
+  if (!active || !payload || payload.length === 0) {
+    return null;
+  }
+
+  const data = payload[0].payload;
+  const { expense, income } = data;
+
+  return (
+    <div className="min-w-[300px] rounded border bg-background p-4">
+      <TooltipRow
+        formatter={formatter}
+        label="Income"
+        value={income}
+        bgColor="bg-emerald-500"
+        textColor="text-emerald-500"
+      />
+
+      <TooltipRow
+        formatter={formatter}
+        label="Expense"
+        value={expense}
+        bgColor="bg-red-500"
+        textColor="text-red-500"
+      />
+
+      <TooltipRow
+        formatter={formatter}
+        label="Balance"
+        value={income - expense}
+        bgColor="bg-gray-100"
+        textColor="text-foreground"
+      />
+    </div>
+  );
+}
+
+function TooltipRow({
+  label,
+  value,
+  bgColor,
+  textColor,
+  formatter,
+}: {
+  label: string;
+  textColor: string;
+  bgColor: string;
+  value: number;
+  formatter: Intl.NumberFormat;
+}) {
+  const formattingFn = useCallback(
+    (value: number) => {
+      return formatter.format(value);
+    },
+    [formatter],
+  );
+
+  return (
+    <div className="flex items-center gap-2">
+      <div className={cn("h-4 w-4 rounded-full", bgColor)} />
+
+      <div className="flex w-full justify-between">
+        <p className="text-sm text-muted-foreground">{label}</p>
+
+        <div className={cn("text-sm font-black", textColor)}>
+          <CountUp
+            duration={0.5}
+            preserveValue
+            end={value}
+            decimals={0}
+            formattingFn={formattingFn}
+            className="text-sm"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
